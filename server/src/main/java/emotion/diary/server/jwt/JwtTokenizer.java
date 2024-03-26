beec8ed6cc3e7d89f3470c4369f04dcfb791911e
JanuaryKim <unool6976@gmail.com>
2024-03-26 13:58:39 +0900

    feat : jwt 생성, jwt 생성 테스트 작성
    - JwtTokenize 생성
    - jwt 관련 의존성 추가
    - jwt 관련 설정값 application.properties에 입력
    - jwt 생성 테스트 작성

---
+++ server/src/main/java/emotion/diary/server/jwt/JwtTokenizer.java
@@ -0,0 +1,78 @@
+package emotion.diary.server.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
+@Getter
+@Component
+public class JwtTokenizer {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.access-token-expiration-minutes}")
+    private int accessTokenExpirationMinutes;
+
+    @Value("${jwt.refresh-token-expiration-minutes}")
+    private int refreshTokenExpirationMinutes;
+
+
+    // access 토큰 생성
+    public String generateAccessToken(Map<String, Object> claims,
+                                      String subject,
+                                      Date expiration,
+                                      String secretKey) {
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(Calendar.getInstance().getTime())
+                .setExpiration(expiration)
+                .signWith(key)
+                .compact();
+    }
+
+    // refresh 토큰 생성
+    public String generateRefreshToken(String subject, Date expiration, String secretKey) {
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(Calendar.getInstance().getTime())
+                .setExpiration(expiration)
+                .signWith(key)
+                .compact();
+    }
+
+    // 검증 후 Claims 반환
+    public Jws<Claims> getClaims(String jws, String secretKey) {
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        Jws<Claims> claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jws);
+        return claims;
+    }
+
+    // 토큰 유효기간 계산
+    public Date getTokenExpiration(int expirationMinutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, expirationMinutes);
+        Date expiration = calendar.getTime();
+
+        return expiration;
+    }
+}
---
+++ server/src/test/java/emotion/diary/server/jwt/JwtTokenizerTests.java
@@ -0,0 +1,53 @@
+package emotion.diary.server.jwt;
+
+import emotion.diary.server.member.entity.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = { //JwtTokenizer만 빈으로 등록
+        JwtTokenizer.class
+})
+public class JwtTokenizerTests {
+
+    @Autowired
+    JwtTokenizer jwtTokenizer;
+
+    @Autowired
+    ApplicationContext ac;
+
+    @BeforeEach
+    void should_PrintToRegisteredBean_Try(){
+        String[] beans = ac.getBeanDefinitionNames();
+
+        for (String bean: beans) {
+            System.out.println("bean: " + bean);
+        }
+    }
+
+    @Test
+    void should_PassVerifyJWT_When_Try(){
+
+        //given
+        Member member = Member.builder().memberId(1L).email("january@google.com").build();
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("memberId", member.getMemberId());
+        claims.put("memberEmail", member.getEmail());
+        String subject = "access_token";
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        //when
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, jwtTokenizer.getSecretKey());
+
+        //then
+        Map<String, Object> getClaim = jwtTokenizer.getClaims(accessToken, jwtTokenizer.getSecretKey()).getBody();
+        assertEquals(getClaim.get("memberEmail"), member.getEmail());
+    }
+}
