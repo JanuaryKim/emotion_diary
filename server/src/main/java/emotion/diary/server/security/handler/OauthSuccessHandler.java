008c99e20315b72384c634b0e5883f0ace86758e
JanuaryKim <unool6976@gmail.com>
2024-03-30 10:15:42 +0900

    feat : 불필요한 import 제거

--- server/src/main/java/emotion/diary/server/security/handler/OauthSuccessHandler.java
+++ server/src/main/java/emotion/diary/server/security/handler/OauthSuccessHandler.java
@@ -12,10 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import java.io.IOException;
 import java.net.URI;
 import java.util.Date;
