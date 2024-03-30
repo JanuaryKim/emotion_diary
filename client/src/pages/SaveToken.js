20178c03427f5b4f2bd6fd6d93c601fc2da7b63b
JanuaryKim <unool6976@gmail.com>
2024-03-30 19:31:25 +0900

    feat : SaveToken 페이지의 수행 내용 useEffect 내부로 이동
    - navigator() 는 useEffect 내부에서만 사용 가능하고, SaveToken 페이지에서
      수행하는 내용은 페이지 렌더링 될때 한번만 수행하면 되므로 전부 이동

--- client/src/pages/SaveToken.js
+++ client/src/pages/SaveToken.js
@@ -1,12 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 // oauth 로그인 후 액세스 토큰을 저장하는 중간 페이지
 const SaveToken = () => {
   console.log("세이브 토큰");
-  const params = new URLSearchParams(window.location.search);
+  const navigator = useNavigate();
 
-  localStorage.setItem(
-    "access_token",
-    JSON.stringify(params.get("access_token"))
-  );
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+
+    localStorage.setItem(
+      "access_token",
+      JSON.stringify(params.get("access_token"))
+    );
+
+    navigator("/", { replace: true });
+  });
 };
 
 export default SaveToken;
